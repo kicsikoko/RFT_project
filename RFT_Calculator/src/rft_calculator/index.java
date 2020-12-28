f63068ecee04a18a1354b0af72ac2a8d45fd8cb3
L0ggeRs <ukeve12@gmail.com>
2020-12-28 19:18:24 +0100

    Update Calculator

--- RFT_Calculator/src/rft_calculator/index.java
+++ RFT_Calculator/src/rft_calculator/index.java
@@ -28,20 +28,143 @@ public class index extends javax.swing.JFrame {
     private void initComponents() {
 
         jPanel1 = new javax.swing.JPanel();
+        jButton15 = new javax.swing.JButton();
+        jButton16 = new javax.swing.JButton();
+        jButton17 = new javax.swing.JButton();
+        jButton10 = new javax.swing.JButton();
+        jButton11 = new javax.swing.JButton();
+        jButton12 = new javax.swing.JButton();
+        jButton18 = new javax.swing.JButton();
+        jButton13 = new javax.swing.JButton();
+        jButton19 = new javax.swing.JButton();
+        jButton5 = new javax.swing.JButton();
+        jButton6 = new javax.swing.JButton();
+        jButton7 = new javax.swing.JButton();
+        jButton8 = new javax.swing.JButton();
+        jButton14 = new javax.swing.JButton();
+        jButton9 = new javax.swing.JButton();
+        jButton1 = new javax.swing.JButton();
+        jButton2 = new javax.swing.JButton();
+        jButton3 = new javax.swing.JButton();
+        jButton4 = new javax.swing.JButton();
+        jLabel1 = new javax.swing.JLabel();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
 
         jPanel1.setLayout(null);
 
+        jButton15.setFont(new java.awt.Font("Tahoma", 1, 12)); // NOI18N
+        jButton15.setText("Del");
+        jPanel1.add(jButton15);
+        jButton15.setBounds(30, 130, 60, 40);
+
+        jButton16.setFont(new java.awt.Font("Tahoma", 1, 18)); // NOI18N
+        jButton16.setText("7");
+        jButton16.setToolTipText("");
+        jPanel1.add(jButton16);
+        jButton16.setBounds(30, 180, 60, 40);
+
+        jButton17.setFont(new java.awt.Font("Tahoma", 1, 18)); // NOI18N
+        jButton17.setText("4");
+        jPanel1.add(jButton17);
+        jButton17.setBounds(30, 230, 60, 40);
+
+        jButton10.setFont(new java.awt.Font("Tahoma", 1, 10)); // NOI18N
+        jButton10.setText("Clear");
+        jPanel1.add(jButton10);
+        jButton10.setBounds(100, 130, 60, 40);
+
+        jButton11.setFont(new java.awt.Font("Tahoma", 1, 18)); // NOI18N
+        jButton11.setText("8");
+        jPanel1.add(jButton11);
+        jButton11.setBounds(100, 183, 60, 40);
+
+        jButton12.setFont(new java.awt.Font("Tahoma", 1, 18)); // NOI18N
+        jButton12.setText("5");
+        jPanel1.add(jButton12);
+        jButton12.setBounds(100, 233, 60, 40);
+
+        jButton18.setFont(new java.awt.Font("Tahoma", 1, 18)); // NOI18N
+        jButton18.setText("1");
+        jPanel1.add(jButton18);
+        jButton18.setBounds(30, 283, 60, 40);
+
+        jButton13.setFont(new java.awt.Font("Tahoma", 1, 18)); // NOI18N
+        jButton13.setText("2");
+        jPanel1.add(jButton13);
+        jButton13.setBounds(100, 280, 60, 40);
+
+        jButton19.setFont(new java.awt.Font("Tahoma", 1, 18)); // NOI18N
+        jButton19.setText("0");
+        jPanel1.add(jButton19);
+        jButton19.setBounds(30, 330, 60, 40);
+
+        jButton5.setFont(new java.awt.Font("Tahoma", 1, 18)); // NOI18N
+        jButton5.setText("+");
+        jPanel1.add(jButton5);
+        jButton5.setBounds(170, 130, 140, 40);
+
+        jButton6.setFont(new java.awt.Font("Tahoma", 1, 18)); // NOI18N
+        jButton6.setText("9");
+        jPanel1.add(jButton6);
+        jButton6.setBounds(170, 183, 60, 40);
+
+        jButton7.setFont(new java.awt.Font("Tahoma", 1, 18)); // NOI18N
+        jButton7.setText("6");
+        jPanel1.add(jButton7);
+        jButton7.setBounds(170, 233, 60, 40);
+
+        jButton8.setFont(new java.awt.Font("Tahoma", 1, 18)); // NOI18N
+        jButton8.setText("3");
+        jPanel1.add(jButton8);
+        jButton8.setBounds(170, 280, 60, 40);
+
+        jButton14.setFont(new java.awt.Font("Tahoma", 1, 18)); // NOI18N
+        jButton14.setText(".");
+        jPanel1.add(jButton14);
+        jButton14.setBounds(100, 330, 60, 40);
+
+        jButton9.setFont(new java.awt.Font("Tahoma", 1, 18)); // NOI18N
+        jButton9.setText("=");
+        jPanel1.add(jButton9);
+        jButton9.setBounds(170, 330, 60, 40);
+
+        jButton1.setFont(new java.awt.Font("Tahoma", 1, 18)); // NOI18N
+        jButton1.setText("*");
+        jPanel1.add(jButton1);
+        jButton1.setBounds(240, 240, 43, 31);
+
+        jButton2.setFont(new java.awt.Font("Tahoma", 1, 18)); // NOI18N
+        jButton2.setText("/");
+        jPanel1.add(jButton2);
+        jButton2.setBounds(240, 290, 43, 31);
+
+        jButton3.setFont(new java.awt.Font("Tahoma", 1, 18)); // NOI18N
+        jButton3.setText("-");
+        jPanel1.add(jButton3);
+        jButton3.setBounds(250, 340, 41, 31);
+
+        jButton4.setFont(new java.awt.Font("Tahoma", 1, 18)); // NOI18N
+        jButton4.setText("%");
+        jPanel1.add(jButton4);
+        jButton4.setBounds(250, 190, 55, 31);
+
+        jLabel1.setIcon(new javax.swing.ImageIcon("E:\\Google Drive (ukeve12)\\EKE\\11. félév\\PTI\\A rendszerfejlesztés technológiája ea\\Projekt\\RFT_project\\RFT_Calculator\\fekete-hatter.jpg")); // NOI18N
+        jLabel1.setText("jLabel1");
+        jPanel1.add(jLabel1);
+        jLabel1.setBounds(0, 0, 360, 520);
+
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
         layout.setHorizontalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addComponent(jPanel1, javax.swing.GroupLayout.DEFAULT_SIZE, 347, Short.MAX_VALUE)
+            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
+                .addGap(0, 0, Short.MAX_VALUE)
+                .addComponent(jPanel1, javax.swing.GroupLayout.PREFERRED_SIZE, 355, javax.swing.GroupLayout.PREFERRED_SIZE))
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addComponent(jPanel1, javax.swing.GroupLayout.DEFAULT_SIZE, 521, Short.MAX_VALUE)
+            .addComponent(jPanel1, javax.swing.GroupLayout.Alignment.TRAILING, javax.swing.GroupLayout.DEFAULT_SIZE, 521, Short.MAX_VALUE)
         );
 
         pack();
@@ -83,6 +206,26 @@ public class index extends javax.swing.JFrame {
     }
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.JButton jButton1;
+    private javax.swing.JButton jButton10;
+    private javax.swing.JButton jButton11;
+    private javax.swing.JButton jButton12;
+    private javax.swing.JButton jButton13;
+    private javax.swing.JButton jButton14;
+    private javax.swing.JButton jButton15;
+    private javax.swing.JButton jButton16;
+    private javax.swing.JButton jButton17;
+    private javax.swing.JButton jButton18;
+    private javax.swing.JButton jButton19;
+    private javax.swing.JButton jButton2;
+    private javax.swing.JButton jButton3;
+    private javax.swing.JButton jButton4;
+    private javax.swing.JButton jButton5;
+    private javax.swing.JButton jButton6;
+    private javax.swing.JButton jButton7;
+    private javax.swing.JButton jButton8;
+    private javax.swing.JButton jButton9;
+    private javax.swing.JLabel jLabel1;
     private javax.swing.JPanel jPanel1;
     // End of variables declaration//GEN-END:variables
 }
